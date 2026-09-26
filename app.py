be9c9727c9c8c2df379a34b5e74fd930b199567a
Rafaellsantos16mc-ai <rafaell.santos16mc@gmail.com>
2026-09-25 21:30:11 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1,14 +1,5 @@
-import os
-
 from flask import Flask
-
 app = Flask(name)
-
-PORT = int(os.environ.get(“PORT”, “8080”))
-
 @app.route(”/”)
 def home():
-return “REEL MAKER ONLINE”
-
-if name == “main”:
-app.run(host=“0.0.0.0”, port=PORT)
+return “OK”
